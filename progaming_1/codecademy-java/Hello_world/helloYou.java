167fba7f5930c7127912173e5b3342882d8869e5
hongphuc0835 <143236536+hongphuc0835@users.noreply.github.com>
2024-03-16 12:06:11 +0700

    Add files via upload

---
+++ progaming_1/codecademy-java/Hello_world/helloYou.java
@@ -0,0 +1,5 @@
+public class helloYou{
+    public static void main(String[] args) {
+        System.out.println("Hello Hong phuc");
+    }
+}
---
+++ progaming_1/codecademy-java/Hello_world/hideAndSeek.java
@@ -0,0 +1,11 @@
+public class hideAndSeek {
+  public static void main(String[] args) {
+    System.out.println("Let's play hide and seek.");
+    System.out.print("Three...");
+    System.out.print("Two...");
+    System.out.println("one...");
+    System.out.println("Ready or not, here I come!");
+
+
+  }
+}
