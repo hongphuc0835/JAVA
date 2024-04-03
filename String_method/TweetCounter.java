9adfe2c1ab80039c87239869aefd59311c0c6684
hongphuc0835 <143236536+hongphuc0835@users.noreply.github.com>
2024-04-03 21:36:10 +0700

    Add files via upload

---
+++ String_method/TweetCounter.java
@@ -0,0 +1,13 @@
+public class TweetCounter {
+  
+	public static void main(String[] args) {
+    
+    String tweet = "Liz Lemon, ninjas are kind of cool... I just dont know any personally. Get on that.";  
+    System.out.println(tweet.length());
+    // What's the character count?
+    // length() do dai chuoi
+    
+    
+  }
+  
+}
